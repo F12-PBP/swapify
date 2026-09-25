02c13396f4dcaf99423bd20d3620b842c192751d
Leficullen <muh.alfi@ui.ac.id>
2026-09-25 10:53:26 +0700

    feat: add dummy pages

--- apps/main/views.py
+++ apps/main/views.py
@@ -4,5 +4,7 @@ from django.views.generic import TemplateView
 class HomeView(TemplateView):
      template_name = "main/home.html"
 
+class DesignSystemView(TemplateView):
+     template_name = "main/ds.html"
 
 # Create your views here.
